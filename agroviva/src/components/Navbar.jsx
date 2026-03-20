2ff28e9c8f29e2065a79fa9856a615e52de0d5f8
andrefxm <andrefxm@gmail.com>
2026-03-20 18:12:29 -0300

    ajusta a estrutura e estilização da Navbar, adiciona link para o Chatbot

--- agroviva/src/components/Navbar.jsx
+++ agroviva/src/components/Navbar.jsx
@@ -2,43 +2,44 @@ import { Link } from "react-router-dom";
 import logoIcon from "../assets/Logo_AgroViva1.PNG";
 
 function Navbar() {
-return (
+  return (
     <header className="topbar">
-    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
         <div className="container">
-        
           {/* Logo */}
-        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <Link className="navbar-brand d-flex align-items-center" to="/">
             <img
-            src={logoIcon}
-            alt="Logo AgroViva Web"
-            className="me-2"
-            style={{ width: "35px", height: "34px", borderRadius: "8px" }}
+              src={logoIcon}
+              alt="Logo AgroViva Web"
+              className="me-2"
+              style={{ width: "35px", height: "34px", borderRadius: "8px" }}
             />
             <span className="fw-bold text-success">AgroViva Web</span>
-        </Link>
+          </Link>
 
           {/* Botão mobile */}
-        <button
+          <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
-        >
+          >
             <span className="navbar-toggler-icon"></span>
-        </button>
+          </button>
 
           {/* Links */}
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
             <ul className="navbar-nav gap-lg-3">
-            
-            <li className="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link fw-bold text-success" to="/">
-                Home
+                  Home
                 </Link>
-            </li>
+              </li>
 
-            <li className="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/impacto">
                   Impacto Social
                 </Link>
@@ -51,11 +52,16 @@ return (
               </li>
 
               <li className="nav-item">
+                <Link className="nav-link" to="/chatbot">
+                  Chatbot
+                </Link>
+              </li>
+
+              <li className="nav-item">
                 <Link className="nav-link" to="/fale">
                   Fale Conosco
                 </Link>
               </li>
-
             </ul>
           </div>
         </div>
